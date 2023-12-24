37b627c0cf983085f2098d73f8af1c7629ad1bb7
mrvijaykumar07 <154747781+mrvijaykumar07@users.noreply.github.com>
2023-12-24 20:55:20 +0530

    Add files via upload

---
+++ Task_1.js
@@ -0,0 +1,21 @@
+
+const navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
+
+const menuItems = document.querySelectorAll('.navbar a');
+menuItems.forEach((item) => {
+  item.addEventListener('mouseenter', () => {
+    item.classList.add('hovered');
+  });
+  item.addEventListener('mouseleave', () => {
+    item.classList.remove('hovered');
+  });
+});
+s
